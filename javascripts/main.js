fe88739a01a22a78517cb31b61ca3cd9d7fc9cd2
Jeff Johnson <johnsonjeff@gmail.com>
2015-05-30 16:39:35 -0700

    Here's some content i prepared earlier

---
+++ javascripts/main.js
@@ -0,0 +1,24 @@
+/// <reference path="../../typings/jquery/jquery.d.ts"/>
+$(function() {
+	var $bloons = $('#bloons img');
+	
+	function randomDuration() {
+		return 3000 + Math.random() * 1000 * 3;
+	}
+	
+	function considerAddDog() {
+		if (Math.random() < 0.1) {
+			var $dog = $('#dog')
+			$dog.css('left', '-400px');
+			$dog.animate({left: '110%'}, randomDuration());
+		}
+	}
+	
+	function animationLoop() {
+		$(this).css('top', '800px');
+		$(this).animate({top: -400}, randomDuration(), animationLoop);	
+		considerAddDog();
+	}
+	
+	$bloons.each(animationLoop);
+});
